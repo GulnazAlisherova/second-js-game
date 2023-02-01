fa397772f63e264ca67840a498fed825f60174f1
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-02-01 20:17:32 +0600

    for variable was added new style: left with variable "random"

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ hole.setAttribute("class", "hole");
 block.setAttribute("id", "block");
 hole.setAttribute("id", "hole");
 var random = Math.floor(Math.random() * 360);
+hole.style.left = random + "px";
 
 game.appendChild(block);
 game.appendChild(hole);
